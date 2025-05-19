e0e1ec7ed5a4a7b68b5f5a96b58564076ea1d65a
AmuroEita <1071307515@qq.com>
2025-05-19 15:32:10 +0800

    update

--- cc_bench.cpp
+++ cc_bench.cpp
@@ -8,7 +8,38 @@
 #include "bench.hpp"
 #include "utils.hpp"
 
+void print_help() {
+    std::cout << "HNSW-Bench Usage:\n"
+              << "Usage: ./cc_bench [options]\n\n"
+              << "Options:\n"
+              << "  -h, --help                Show this help message\n"
+              << "  -d, --dataset_name NAME   Name of the dataset\n"
+              << "  -t, --data_type TYPE      Type of the data\n"
+              << "  -p, --data_path PATH      Path to the data file\n"
+              << "  -q, --query_path PATH     Path to the query file\n"
+              << "  -b, --batch_res_path PATH Path to save batch results\n"
+              << "  -i, --begin_num NUM       Initial number of points to build\n"
+              << "  -m, --max_elements NUM    Maximum number of elements\n"
+              << "  -w, --write_ratio RATIO   Write ratio (0-1)\n"
+              << "  -s, --batch_size NUM      Batch size for processing\n"
+              << "  -r, --recall_at NUM       k value for recall calculation\n"
+              << "  -R, --R NUM               R parameter for index\n"
+              << "  -L, --Lb NUM              Lb parameter for index\n"
+              << "  -l, --Ls NUM              Ls parameter for search\n"
+              << "  -n, --num_threads NUM     Number of threads\n"
+              << "  -g, --gt_path PATH        Path to the ground truth file\n"
+              << "  -o, --stat_path PATH      Path to save statistics\n\n"
+              << "Example:\n"
+              << "  ./cc_bench -d sift -t float -p data.bin -q query.bin -b results/ -i 10000 -m 1000000 "
+              << "-w 0.5 -s 1000 -r 10 -R 16 -L 32 -l 100 -n 16 -g gt.bin -o stats.csv\n";
+}
+
 int main(int argc, char *argv[]) {
+    if (argc == 1 || (argc == 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0))) {
+        print_help();
+        return 0;
+    }
+
     int retval = PAPI_library_init(PAPI_VER_CURRENT);
     if (retval != PAPI_VER_CURRENT) {
         std::cerr << "PAPI library init error: " << PAPI_strerror(retval)
@@ -23,61 +54,46 @@ int main(int argc, char *argv[]) {
     size_t recall_at = 10, R = 16, Ls = 50, Lb = 50,
            num_threads = std::thread::hardware_concurrency();
 
-    struct option long_options[] = {{"dataset_name", required_argument, 0, 0},
-                                    {"data_type", required_argument, 0, 0},
-                                    {"data_path", required_argument, 0, 0},
-                                    {"query_path", required_argument, 0, 0},
-                                    {"batch_res_path", required_argument, 0, 0},
-                                    {"begin_num", required_argument, 0, 0},
-                                    {"max_elements", required_argument, 0, 0},
-                                    {"write_ratio", required_argument, 0, 0},
-                                    {"batch_size", required_argument, 0, 0},
-                                    {"recall_at", required_argument, 0, 0},
-                                    {"R", required_argument, 0, 0},
-                                    {"Lb", required_argument, 0, 0},
-                                    {"Ls", required_argument, 0, 0},
-                                    {"dim", required_argument, 0, 0},
-                                    {"num_threads", required_argument, 0, 0},
-                                    {"gt_path", required_argument, 0, 0},
-                                    {"stat_path", required_argument, 0, 0},
+    struct option long_options[] = {{"dataset_name", required_argument, 0, 'd'},
+                                    {"data_type", required_argument, 0, 't'},
+                                    {"data_path", required_argument, 0, 'p'},
+                                    {"query_path", required_argument, 0, 'q'},
+                                    {"batch_res_path", required_argument, 0, 'b'},
+                                    {"begin_num", required_argument, 0, 'i'},
+                                    {"max_elements", required_argument, 0, 'm'},
+                                    {"write_ratio", required_argument, 0, 'w'},
+                                    {"batch_size", required_argument, 0, 's'},
+                                    {"recall_at", required_argument, 0, 'r'},
+                                    {"R", required_argument, 0, 'R'},
+                                    {"Lb", required_argument, 0, 'L'},
+                                    {"Ls", required_argument, 0, 'l'},
+                                    {"num_threads", required_argument, 0, 'n'},
+                                    {"gt_path", required_argument, 0, 'g'},
+                                    {"stat_path", required_argument, 0, 'o'},
                                     {0, 0, 0, 0}};
 
     int option_index = 0;
     int c;
-    while ((c = getopt_long(argc, argv, "", long_options, &option_index)) !=
-           -1) {
-        if (c == 0) {
-            std::string opt_name = long_options[option_index].name;
-            if (opt_name == "dataset_name")
-                dataset_name = optarg;
-            else if (opt_name == "data_type")
-                data_type = optarg;
-            else if (opt_name == "data_path")
-                data_path = optarg;
-            else if (opt_name == "query_path")
-                query_path = optarg;
-            else if (opt_name == "batch_res_path")
-                batch_res_path = optarg;
-            else if (opt_name == "begin_num")
-                begin_num = std::stoul(optarg);
-            else if (opt_name == "write_ratio")
-                write_ratio = std::stof(optarg);
-            else if (opt_name == "batch_size")
-                batch_size = std::stoul(optarg);
-            else if (opt_name == "recall_at")
-                recall_at = std::stoul(optarg);
-            else if (opt_name == "R")
-                R = std::stoul(optarg);
-            else if (opt_name == "Lb")
-                Lb = std::stoul(optarg);
-            else if (opt_name == "Ls")
-                Ls = std::stoul(optarg);
-            else if (opt_name == "num_threads")
-                num_threads = std::stoul(optarg);
-            else if (opt_name == "gt_path")
-                gt_path = optarg;
-            else if (opt_name == "stat_path")
-                stat_path = optarg;
+    while ((c = getopt_long(argc, argv, "d:t:p:q:b:i:m:w:s:r:R:L:l:D:n:g:o:h", long_options, &option_index)) != -1) {
+        switch (c) {
+            case 'd': dataset_name = optarg; break;
+            case 't': data_type = optarg; break;
+            case 'p': data_path = optarg; break;
+            case 'q': query_path = optarg; break;
+            case 'b': batch_res_path = optarg; break;
+            case 'i': begin_num = std::stoul(optarg); break;
+            case 'm': /* max_elements */ break;
+            case 'w': write_ratio = std::stof(optarg); break;
+            case 's': batch_size = std::stoul(optarg); break;
+            case 'r': recall_at = std::stoul(optarg); break;
+            case 'R': R = std::stoul(optarg); break;
+            case 'L': Lb = std::stoul(optarg); break;
+            case 'l': Ls = std::stoul(optarg); break;
+            case 'n': num_threads = std::stoul(optarg); break;
+            case 'g': gt_path = optarg; break;
+            case 'o': stat_path = optarg; break;
+            case 'h': print_help(); return 0;
+            case '?': return 1;
         }
     }
 
